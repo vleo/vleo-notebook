6ab5d2f319fcec8907f612af352a5a3c51ca7358
Vassili Leonov <vleo@linuxmedialabs.com>
2011-06-15 18:21:28 +0400

    added genrandpass.pl

---
+++ bin/genrandpass.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+$lp=10;
+
+#srand(33);
+
+@L=split //,'01234567890qwertyuiopasdfghjkzxcvbnmQWERTYUIPLKJHGFDSAZXCVBNM+-?!$%&@_';
+
+#@L=split //,'0123456789qwerty';
+$sl=int(@L);
+#print int(@L),"\n"; 
+#print join(":",@L),"\n";
+
+while ($i++ < $lp)
+{
+$r1=int(rand($sl));
+$d[$r1]++;
+$p = $p . $L[$r1];
+}
+
+for ($j=0;$j<$sl;$j++)
+{
+#printf "%2d %s  %3d %s\n", $j, $L[$j], $d[$j] , '*' x $d[$j];
+}
+
+print $p,"\n";
