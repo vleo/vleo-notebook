9ae020d35dc64422579907789de9b90827de8aa5
Vassili Leonov <vleolml@gmail.com>
2011-09-23 11:07:14 +0400

    reversetest for MD

--- mduml_study/reversetest/src/A.cpp
+++ mduml_study/reversetest/src/A.cpp
@@ -1,33 +1,10 @@
 //@(#) A.cpp
 
 
-extern int errno;
-
-B::B( const int n ) : a(n)
-{
-	
-}
-
-int gVar = 1;
-
+#include "A.h"
 int A::f( const int i )
 {
 	return 0;
-}
-
-int main( const int argc, const char * * argv )
-{
-	return 0;
-}
-
-int B::g( )
-{
-	return 0;
-}
-
-void B::s( const int n )
-{
-	
 }
 
 void A::setC( const B & v )
