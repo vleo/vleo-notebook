03a8a9218fa3d64406a66259eb91b6ba13021db5
Vassili Leonov <vleo@linuxmedialabs.com>
2012-04-06 00:25:30 +0400

    better comments for shell template with options

--- templates/shell_script_getopt.sh
+++ templates/shell_script_getopt.sh
@@ -6,8 +6,10 @@ if [ $? != 0 ] ; then TEMP="-h --" ; fi
 eval set -- "$TEMP"
 while true ; do
         case "$1" in
+# change options here according to your needs
            -A ) shift; OPT_A="-A" ;;
            -i ) shift;  INPUT_FILE=$1; shift ;;
+# change USAGE text (help) below
            -h ) cat <<EOT
 Usage: $0 -i <input file> [-A]
 Options:
@@ -19,6 +21,8 @@ EOT
            --) shift ; break ;;
         esac
 done
+# change to check YOUR options consistency here
 if [ -z "$INPUT_FILE" ]; then echo "Must specify -i <filename>"; $0 -h; exit 1; fi
 ###### end of options processing ######
-cat $OPT_A $INPUT_FILE
+cat $OPT_A $INPUT_FILE # <-- this is only an example of options use, remove
+
