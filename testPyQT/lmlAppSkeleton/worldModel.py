c17c26a93c83fd9e2ea2f4eaf4806be6b8eb5e16
Vassili Leonov <vleo@linuxmedialabs.com>
2018-05-10 14:01:20 +0300

    added some initial notes for universal modelling application

---
+++ testPyQT/lmlAppSkeleton/worldModel.py
@@ -0,0 +1,4 @@
+# this is QObject based code that
+# - holds realWorld values
+# - connects values to slots on the worldView
+# - exports slots
---
+++ testPyQT/lmlAppSkeleton/worldView.py
@@ -0,0 +1,5 @@
+# this is PyQt based code that
+# - draws GUI
+# - exports slots to connect to indicators
+# - exports methods to connect to controls' slots on worldModel
+# - creates and runs PyQt event loop
